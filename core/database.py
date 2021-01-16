958d20ca86f5ccc213f515c1bd21d264303808b4
Joseph Duclos <43616909+CazCapone@users.noreply.github.com>
2021-01-15 17:32:37 -0700

    Delete database.py

--- core/database.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-
-# Database connection
-db_connection = MySQLdb.connect(
-  	host="webdev2.city.tucsonaz.gov",
-  	user="assoc_db",
-  	passwd="hxGZFifNn55kTQD8",
-    database="associations"
-)
